c2f4bc8d004adefbdcf92d5d713c8d1ceb39bf0f
Fabio Moro <fabio.moro2000@gmail.com>
2022-10-04 15:46:57 +0200

    aggiunto stile

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -26,7 +26,7 @@ namespace la_mia_pizzeria_model.Controllers
         public IActionResult Details(int id)
         {
             List<Pizza> pizzaList = new List<Pizza>();
-            pizzaList.Add(new Pizza("Margherita", "Mozzarella e Pomodoro", "https://www.tavolartegusto.it/wp/wp-content/uploads/2020/05/Pizza-napoletana-Ricetta-della-Pizza-Napoletana-Pizza-Margherita.jpg", 5.99));
+            pizzaList.Add(new Pizza("Margherita", "Mozzarella, Pomodoro e Basilico", "https://www.tavolartegusto.it/wp/wp-content/uploads/2020/05/Pizza-napoletana-Ricetta-della-Pizza-Napoletana-Pizza-Margherita.jpg", 5.99));
             pizzaList.Add(new Pizza("Napoli", "Mozzarella, Pomodoro e Alici", "https://www.donnamoderna.com/content/uploads/2021/08/pizza-napoli.jpg", 6.99));
             pizzaList.Add(new Pizza("Diavola", "Salame piccante", "https://www.misya.info/wp-content/uploads/2007/08/Pizza-alla-diavola.jpg", 7.99));
             pizzaList.Add(new Pizza("Ortolana", "Verdura varia", "https://www.agrodolce.it/app/uploads/2018/01/pizza-ortolana-a1742-16.jpg", 7.99));
